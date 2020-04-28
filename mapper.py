0d183896c7334df326d3c784a5bc3f8d932c0477
adfr96 <alexdellorco@gmail.com>
2020-04-28 11:24:57 +0200

    add data streaming in json format & pre_min calc

--- mapper.py
+++ mapper.py
@@ -2,6 +2,19 @@
 """mapper.py"""
 
 import sys
+import json
+
+def toJson(azione):
+    dic = {   "ticker" : azione[0],
+     "open" : azione[1], 
+     "close" : azione[2],
+     "low" : azione[4],
+     "high" : azione[5],
+     "volume" : azione[6],
+    "date" : azione[7],
+    }
+    return json.dumps(dic)
+
 
 for line in sys.stdin:
     azione = line.split(',')
@@ -10,5 +23,5 @@ for line in sys.stdin:
     anno_azione = azione[7].split('-')[0]
     if int(anno_azione)>=2008:
         #print(azione[0],azione)
-        print('%s\t%s' % (azione[0], azione))
+        print('%s\t%s' % (azione[0], toJson(azione)))
 
--- reducer.py
+++ reducer.py
@@ -2,27 +2,26 @@
 """reducer.py"""
 
 import sys
+import json
 dizionario = {}
 for line in sys.stdin:
     #Costruiamo un dizionario con chiave il simbolo dell'azione e
     # valore l'azione che comprende quindi tutti i campi
     line = line.split('\t')
     simbolo = line[0]
-    azione = line[1]
-    #azione = {'prezzo_min':line[1][4]}
+    azione = json.loads(line[1])
 
     if dizionario.get(simbolo):
        dizionario[simbolo].append(azione)
     else:
         dizionario[simbolo] = [azione]
 
+for k in dizionario:
 
-
-
-    #calcolo del prezzo minimo per ogni azione:
-    """
-    if dizionario.get(simbolo):
-        if (int)dizionario[simbolo]>costo_attuale
-    """
-
-print (dizionario)
+    # è equivalente al for sotto, ma visto che dovremmo fare altre cose conviene il for esplicito
+    #prezzi_min = [float(a['low']) for a in dizionario[k]] 
+    
+    prezzi_min = [] 
+    for a in dizionario[k]:
+        prezzi_min.append(float(a['low']))
+    print(f'{k}\tprezzo_min:{min(prezzi_min)}')
