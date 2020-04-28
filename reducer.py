d6148301453ede2213059fb9d301010f159a8193
adfr96 <alexdellorco@gmail.com>
2020-04-28 12:14:55 +0200

    add hiest_price calc

--- reducer.py
+++ reducer.py
@@ -10,18 +10,37 @@ for line in sys.stdin:
     line = line.split('\t')
     simbolo = line[0]
     azione = json.loads(line[1])
-
+    """
+    formato dell'oggetto caricato:
+    {
+        "ticker" : "GDR",
+        "open" : "9.46724, 
+        "close" : "9.743632",
+        "low" : "9.141",
+        "high" : "10.287228",
+        "volume" : "57393",
+        "date" : "2017-04-27"
+    }
+    """
+    #Riempimento del dizionario
     if dizionario.get(simbolo):
        dizionario[simbolo].append(azione)
     else:
         dizionario[simbolo] = [azione]
 
+#Iterarazione sul dizionario per calcolare i vari campi richiesti
 for k in dizionario:
 
     # è equivalente al for sotto, ma visto che dovremmo fare altre cose conviene il for esplicito
     #prezzi_min = [float(a['low']) for a in dizionario[k]] 
     
     prezzi_min = [] 
+    prezzi_max = []
     for a in dizionario[k]:
         prezzi_min.append(float(a['low']))
-    print(f'{k}\tprezzo_min:{min(prezzi_min)}')
+        prezzi_max.append(float(a['high']))
+
+    lowest_price = min(prezzi_min)
+    hieght_price = max(prezzi_max)
+
+    print(f'{{\nsimbolo:{k}\nprezzo_min:{lowest_price}\nprezzo_massimo:{hieght_price}\n}}')
