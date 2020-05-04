828c7fc0c32e0b47bb2e4761eeb49b32d415b32d
adfr96 <alexdellorco@gmail.com>
2020-05-04 19:27:44 +0200

    add rdd convert in job2_spark

--- job1/spark/job1_spark.py
+++ job1/spark/job1_spark.py
@@ -2,7 +2,15 @@
 import pyspark as ps
 
 spark = ps.sql.SparkSession.builder.appName("Python Spark SQL basic example").config("spark.some.config.option", "some-value").getOrCreate()
-df = spark.read.load("/home/adfr/Documenti/python-BigData/progetto1/csv_progetto/test-progetto.csv",
-                     format="csv", sep=",", inferSchema="true", header="true")
-df.filter(lambda r_df: r_df['2007-02-20'].split('-')[0]>'2008')
-df.show()
+#df = spark.read.load("/home/adfr/Documenti/python-BigData/progetto1/csv_progetto/test-progetto.csv",
+                   #  format="csv", sep=",", inferSchema="true", header="true")
+
+df=spark.read.csv('/home/adfr/Documenti/python-BigData/progetto1/csv_progetto/test-progetto.csv',inferSchema="true", header="true")
+#df.filter(df.date>'2008').show()
+
+azioni = df.rdd
+azioni = azioni.filter(lambda a: a['date']>'2008').map(lambda a: (a[0],a)).groupByKey()
+azioni.foreach(lambda a: print (a))
+#azioni.collect()
+#azioni.saveAsTextFile("output.txt")
+#output.saveAsTextFile("output.txt")
