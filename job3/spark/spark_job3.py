f2edf83dff3da46febc5e56cd8f0a741f51287f3
adfr96 <alexdellorco@gmail.com>
2020-05-13 19:30:58 +0200

    work job3 spark

--- job3/spark/spark_job3.py
+++ job3/spark/spark_job3.py
@@ -12,8 +12,31 @@ df_azioni = df_azioni.filter(df_azioni.date > '2016-01-01').join(df_aziende,on="
                                                     .drop('open').drop('adj_close')\
                                                     .drop('low').drop('high').drop('volume')\
                                                     .drop('exchange').drop('sector').drop('industry')
+def reduce_variazione_annua(v1,v2):
+    res = {}
+    if(v1['first_date']<v2['first_date']):
+         res['first_date'] = v1['first_date']
+         res['first_price'] = v1['first_price']
+    else:
+        res['first_date'] = v2['first_date']
+        res['first_price'] = v2['first_price']
+    
+    if(v1['last_date']>v2['last_date']):
+         res['last_date'] = v1['last_date']
+         res['last_price'] = v1['last_price']
+    else:
+        res['last_date'] = v2['last_date']
+        res['last_price'] = v2['last_price']
+    return res
 
 rdd_base = df_azioni.rdd
-
+for anno in C.ANNI:
+    rdd_variazione = rdd_base.filter(lambda row: row['date'][0:4]==str(anno)).map(lambda row: (row['name'],{\
+        "first_date":row['date'],"first_price":row['close'],"last_date":row['date'],"last_price":row['close']}))
+    rdd_variazione = rdd_variazione.reduceByKey(reduce_variazione_annua)
+    rdd_variazione = rdd_variazione.map(lambda c: (c[0],100*(c[1]['first_price']-c[1]['last_price'])/c[1]['first_price']))
+    rdd_variazione.foreach(lambda a: print(a[0],a[1],"%"))
+"""
 print("\nresult finale:\n")
 rdd_base.foreach(lambda a: print(a))
+"""
