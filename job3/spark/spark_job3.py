06b4035976de9660e1a147b5076adfd60a86dac8
adfr96 <alexdellorco@gmail.com>
2020-05-14 13:31:07 +0200

    work on job3 spark

--- job3/spark/spark_job3.py
+++ job3/spark/spark_job3.py
@@ -3,7 +3,7 @@ import costanct as C
 
 spark = SparkSession.builder.appName("Python Spark job 3 for Big Data project").config("spark.some.config.option", "some-value").getOrCreate()
 
-df_azioni=spark.read.csv('/home/adfr/Documenti/python-BigData/progetto1/csv_progetto/test-medio.csv',inferSchema="true", header="true")
+df_azioni=spark.read.csv('/home/adfr/Documenti/python-BigData/progetto1/csv_progetto/historical_stock_prices.csv',inferSchema="true", header="true")
 
 df_aziende = spark.read.csv('/home/adfr/Documenti/python-BigData/progetto1/csv_progetto/historical_stocks.csv',inferSchema="true", header="true")
 
@@ -34,8 +34,32 @@ for anno in C.ANNI:
     rdd_variazione = rdd_base.filter(lambda row: row['date'][0:4]==str(anno)).map(lambda row: (row['name'],{\
         "first_date":row['date'],"first_price":row['close'],"last_date":row['date'],"last_price":row['close']}))
     rdd_variazione = rdd_variazione.reduceByKey(reduce_variazione_annua)
-    rdd_variazione = rdd_variazione.map(lambda c: (c[0],100*(c[1]['first_price']-c[1]['last_price'])/c[1]['first_price']))
-    rdd_variazione.foreach(lambda a: print(a[0],a[1],"%"))
+    #rdd_variazione(k,v)-> k:nome_azione, v: variazione
+    rdd_variazione = rdd_variazione.map(lambda c: (c[0],int(100*(c[1]['first_price']-c[1]['last_price'])/c[1]['first_price'])))
+    if(anno == C.A1):
+        rdd_tot = rdd_variazione
+    else:
+        rdd_tot = rdd_tot.union(rdd_variazione)
+    #rdd_variazione.foreach(lambda a: print(a[0],a[1],"%"))
+#rdd_tot(k,v)-> k:nome_azione, v: [variazione1,variazione2,variazione3]
+rdd_tot = rdd_tot.groupByKey()
+#inverto i valori della coppia e ragruppo, così aggrego per Tripletta
+rdd_tot = rdd_tot.map(lambda row: (row[1],row[0])).groupByKey()
+#rdd_tot.saveAsTextFile("output")
+list_tot = rdd_tot.collect()
+#list_tot = sorted(list_tot, key= lambda a: a[0],reverse=True)
+file_name = "output.txt"
+fo = open(file_name,'w')
+for (k,v) in list_tot:
+    for i in k:
+        fo.write(str(i))
+        fo.write("%  ")
+    fo.write("{")
+    for a in v:
+        fo.write(a)
+        fo.write(" | ")
+    fo.write("}\n")
+fo.close()
 """
 print("\nresult finale:\n")
 rdd_base.foreach(lambda a: print(a))
