9a7c5e41020a2316bdb1b5fb6551c1ea41fcc737
adfr96 <alexdellorco@gmail.com>
2020-05-21 10:35:19 +0200

    upload enlarge file

---
+++ enlarge.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+with open("../csv_progetto/historical_stock_prices.csv",'r') as f:
+    with open("../csv_progetto/historical_stock_prices_2.csv",'a') as f1: 
+        for line in f:
+            ticker,_,close,_,_,_,_,data = line.split(',')
+            if(data>'2008-01-01'):
+                f1.write(line)
+
+ 
